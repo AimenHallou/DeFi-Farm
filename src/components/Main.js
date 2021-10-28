65083387f0f268d3134c51ced7b760dbaac51893
Aimen <hdmen55@hotmail.com>
2021-10-28 16:57:32 -0400

    Added staking on local side

--- src/components/Main.js
+++ src/components/Main.js
@@ -24,7 +24,13 @@ class Main extends Component {
 
                 <div className="card mb-4">
                     <div className="card-body">
-                        <form className="mb-3">
+                        <form className="mb-3" onSubmit={(event)=>{
+                            event.preventDefault()
+                            let amount
+                            amount = this.input.value.toString()
+                            amount = window.web3.utils.toWei(amount, "Ether")
+                            this.props.stakeTokens(amount)
+                        }}>
                             <div className="card-body">
                                 <label className="float-left"><b>Stake Tokens</b></label>
                                     <span className=" float-right text-muted">
@@ -33,10 +39,11 @@ class Main extends Component {
 
                             </div>
                             <div className="input-group mb-4">
-                                <input type="text" className="form-control form-control-lg" placeholder="0" required/>
+                                <input type="text" ref={(input)=> {this.input = input}} className="form-control form-control-lg" placeholder="0" required/>
                                 <div className="input-group-append">
                                     <div className="input-group-text">
                                         <img src={dai} height="32" alt=""/>
+                                        &nbsp;&nbsp;&nbsp; mDAI
                                     </div>
                                 </div>
                             </div>
